afde8f81401b3beb236c17f29428e6faae6e1b83
Praneeth <praneethshetty626@gmail.com>
2023-06-11 13:17:36 +0530

    cross origin update 2.0

---
+++ src/main/java/com/praneeth/productService/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.praneeth.productService;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@ComponentScan
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("*")
+                .allowedOrigins("https://foodyres.web.app")
+                .allowedMethods("*"); // Add other HTTP methods if needed
+    }
+}
